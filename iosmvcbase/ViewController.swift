cce237de58724be1028bcf38248465e1918d5d1c
HUNG HOANG <anhhungnxh@gmail.com>
2018-07-10 00:45:23 +0900

    Initial Commit

---
+++ iosmvcbase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosmvcbase
+//
+//  Created by HUNG HOANG on 2018/07/10.
+//  Copyright © 2018 HUNG HOANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
