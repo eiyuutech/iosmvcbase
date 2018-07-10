120d721149ed4b07acacdbfde1110b10145434f7
HUNG HOANG <anhhungnxh@gmail.com>
2018-07-10 22:16:41 +0900

    Creating a separate build scheme for dev, stage, production

--- iosmvcbase/ViewController.swift
+++ iosmvcbase/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.testEnv()
     }
-
+    func testEnv() -> Void {
+//        #if DEVELOP
+//            self.labelName.text = "hello em"
+//        #elseif STAGING
+//            self.labelName.text = "hello co"
+//        #else
+//            self.labelName.text = "hello ku"
+//        #endif
+        let hoge = Bundle.main.object(forInfoDictionaryKey: "keiyuuhero") as! String
+        self.labelName.text = hoge
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
